cb1377c114f3df5771b7e9a61bd13cd61ef00772
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-12-13 13:07:22 -0500

    added js to add name input to table; added id to add project button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
 //Add name form element
-var name = $('input[name="name-input"]');
+var nameInput = $('input[name="name-input"]');
 //grab table 
+var table = $('#form-table');
 
 //create function that handles the form submit
+function handleFormSubmit(event) {
+event.preventDefault();
+
+var tableInput = $('<td>');
+
+tableInput.text(nameInput.val());
+
+tableInput.append(table);
+
+$('input[type="name"]').val('');
+
+}
 //event.preventDefault
 //create a table row
 //adds text to it with the name input
-//append neew element to thee final table
+//append new element to the final table
 //clear name element
 //add submit event
 
@@ -21,3 +34,5 @@ function currentTime() {
     }, 1)
 }
 currentTime();
+
+table.on('submit', handleFormSubmit);
