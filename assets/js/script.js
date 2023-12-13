23145c80b19b6373162bf74097218abf71d9be81
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2023-12-13 13:01:30 -0500

    added name to input for project name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+//Add name form element
+var name = $('input[name="name-input"]');
+//grab table 
+
+//create function that handles the form submit
+//event.preventDefault
+//create a table row
+//adds text to it with the name input
+//append neew element to thee final table
+//clear name element
+//add submit event
+
+
 var date = dayjs().format('MMM D, YYYY');
 $('#date').text(date);
 
